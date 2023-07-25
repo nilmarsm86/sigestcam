03baa0644d847a6341411b5bb15f81e2dc9840f5
Nimar Sanchez Muguercia <nilmarsm86@gmail.com>
2023-07-25 17:29:17 -0400

    25-07 tarde

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -14,43 +14,5 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class RegistrationController extends AbstractController
 {
-    #[Route('/register', name: 'app_register')]
-    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, UserRepository $userRepository, RoleRepository $rolRepository): Response
-    {
-        if(!is_null($this->getUser())){
-            return $this->redirectToRoute('app_home');
-        }
 
-        $error = false;
-        $message = '';
-
-        $form = $this->createForm(RegistrationFormType::class);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $roleUser = $rolRepository->findOneBy(['name' => 'ROLE_USER']);
-
-            /** @var RegistrationForm $dto */
-            $dto = $form->getData();
-            $user = $dto->toEntity();
-            $user->register($userPasswordHasher, $roleUser);
-
-            try {
-                $userRepository->save($user, true);
-                // do anything else you need here, like send an email
-                $this->addFlash('success', 'Se a registrado correctamente en el sistema. Espere que le activen el usuario para autenticarse.');
-
-                return $this->redirectToRoute('app_login');
-            }catch (\Exception $exception){
-                $error = true;
-                $message = 'Ha ocurrido un error al registrar al usuario. Revise bien los datos.';
-            }
-        }
-
-        return $this->render('registration/register.html.twig', [
-            'form' => $form->createView(),
-            'error' => $error,
-            'message' => $message
-        ]);
-    }
 }
--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,9 +2,15 @@
 
 namespace App\Controller;
 
+use App\DTO\RegistrationForm;
+use App\Form\RegistrationFormType;
+use App\Repository\RoleRepository;
+use App\Repository\UserRepository;
 use LogicException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
@@ -29,5 +35,45 @@ class SecurityController extends AbstractController
     public function logout(): void
     {
         throw new LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+
+    #[Route('/register', name: 'app_register')]
+    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, UserRepository $userRepository, RoleRepository $rolRepository): Response
+    {
+        if(!is_null($this->getUser())){
+            return $this->redirectToRoute('app_home');
+        }
+
+        $error = false;
+        $message = '';
+
+        $form = $this->createForm(RegistrationFormType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $roleUser = $rolRepository->findOneBy(['name' => 'ROLE_USER']);
+
+            /** @var RegistrationForm $dto */
+            $dto = $form->getData();
+            $user = $dto->toEntity();
+            $user->register($userPasswordHasher, $roleUser);
+
+            try {
+                $userRepository->save($user, true);
+                // do anything else you need here, like send an email
+                $this->addFlash('success', 'Se a registrado correctamente en el sistema. Espere que le activen el usuario para autenticarse.');
+
+                return $this->redirectToRoute('app_login');
+            }catch (\Exception $exception){
+                $error = true;
+                $message = 'Ha ocurrido un error al registrar al usuario. Revise bien los datos.';
+            }
+        }
+
+        return $this->render('security/register.html.twig', [
+            'form' => $form->createView(),
+            'error' => $error,
+            'message' => $message
+        ]);
     }
 }
